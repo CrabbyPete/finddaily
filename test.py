b8a19f38ffe325eaad6a111915b1cf50a07a6423
Pete Douma <pete.douma@gmail.com>
2014-06-02 15:51:02 -0400

    add tests

--- test.py
+++ test.py
@@ -1,19 +1,28 @@
-# flask_testing/test_base.py
-from flask.ext.testing import TestCase
+import os
+import main
+import unittest
+import tempfile
 
-from . import app, db
-
-
-class BaseTestCase(TestCase):
-    """A base test case for flask-tracking."""
-
-    def create_app(self):
-        app.config.from_object('config.TestConfiguration')
-        return app
+class FlaskrTestCase(unittest.TestCase):
 
     def setUp(self):
-        db.create_all()
+        main.app.config['TESTING'] = True
+        self.app = main.app.test_client()
 
+    def signin(self, username, password):
+        return self.app.post('/signin', data=dict(
+            username=username,
+            password=password
+        ), follow_redirects=True)
+
+
+    def test_login_logout(self):
+        rv = self.signin('peted','drd00m')
+        assert 'My Account' in rv.data
+    
     def tearDown(self):
-        db.session.remove()
-        db.drop_all()
+        os.close(self.db_fd)
+        os.unlink(main.app.config['DATABASE'])
+
+if __name__ == '__main__':
+    unittest.main()
